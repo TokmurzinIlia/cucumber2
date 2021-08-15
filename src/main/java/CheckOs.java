33975a7890c2b6c79ff14d663e5b34c7ce9e1046
User <ilia.tokmurzin@gmail.com>
2021-08-15 20:58:03 +0300

    First commit

---
+++ src/main/java/CheckOs.java
@@ -0,0 +1,16 @@
+
+
+public class CheckOs {
+
+public static String checkOs(){
+    String str = System.getProperty("os.name");
+    return str;
+}
+public static String getFeaturesWay(){
+    String feature = null;
+
+    if(checkOs().contains("Windows")){
+        feature = "src/test/resources/features";
+    } else if (checkOs().contains("Linux")){feature = "src\test\resources\features";}
+return feature;}
+}
